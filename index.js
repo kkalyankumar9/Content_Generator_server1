2e76b4b135b5bdbdc67dc8d51887ca96cb072e16
kkalyankumar9 <kummarikalyan010@gmail.com>
2023-10-29 19:28:21 +0530

    server

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const axios = require('axios');
-const cors=require("cors")
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 const PORT = process.env.PORT||8000;
 
